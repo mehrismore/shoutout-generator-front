5500b34c008d79e9ff1c9786fdc9bde336bca43f
TessavWalstijn <twalstijn@outlook.com>
2021-10-14 22:37:44 +0200

    Base project

---
+++ source/pages/introduction/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import WelcomeBackground from '@source/components/welcome-background/welcome-background'
+
+const Intro = () => {
+  return <WelcomeBackground></WelcomeBackground>
+}
+
+export default Intro
