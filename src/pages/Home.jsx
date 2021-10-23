cce734cadf1f7d01fffbac45929079683e4dae32
Mehrnoosh Beniss <mehrnoosh.beniss@gmail.com>
2021-10-23 15:13:04 +0200

    feat: styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,15 @@ import React from "react";
 
 export const Home = () => {
   return (
-    <div className="container flex flex-col items-center text-lg text-white">
-      Shoutout to YOU!
-      <div className="">
+    <div className="container mx-auto m-20 flex flex-col items-center">
+    <div className="text-indigo-500 text-base">She# Hackathon 2021</div>
+      <div className="flex flex-col items-center text-lg text-yellow-500 text-9xl font-extrabold mt-20">
+        Shoutout to you!
         <button
           type="button"
-          className="inline-flex items-center px-6 py-3 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          className="inline-flex items-center px-8 py-4 mt-10 border shadow-sm text-6xl font-medium bg-indigo-600 text-yellow-50 rounded-md hover:bg-indigo-400 text-yellow-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-400"
         >
-          Send me some love!
+          Send!
         </button>
       </div>
     </div>
