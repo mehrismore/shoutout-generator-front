ddff02adc80ab629cc74e33d2c90087d7a1529a2
Mehrnoosh Beniss <mehrnoosh.beniss@gmail.com>
2021-10-24 15:02:32 +0200

    feat: add style to shoutout + robot img

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import robot from "../assets/robot.png";
 
 export const Home = () => {
   const [quote, setQuote] = useState({});
@@ -14,19 +15,32 @@ export const Home = () => {
     let text = quote.text;
     let author = quote.author;
     return (
-      <div>
+      <button
+        type="button"
+        className="inline-flex
+          items-center
+          px-8
+          py-4
+          mt-16
+          border
+          shadow-sm
+          text-3xl
+          font-medium
+          bg-yellow-50
+          text-purple-400
+          rounded-lg"
+      >
         {text} {author ? `— ${author}` : null}
-      </div>
+      </button>
     );
   };
   console.log(quote.author);
   return (
-    <div className="container mx-auto m-20 flex flex-col items-center">
+    <div className="container mx-auto m-10 flex flex-col items-center">
       <div className="text-indigo-900 text-lg font-medium">
         She# + Troopl Hackathon 2021
       </div>
-      {displayQuote()}
-      <div className="flex flex-col items-center text-lg text-purple-700 text-9xl font-extrabold mt-28">
+      <div className="flex flex-col items-center text-lg text-purple-700 text-9xl font-extrabold mt-16">
         Shoutout to you!
         <button
           type="button"
@@ -34,10 +48,10 @@ export const Home = () => {
           items-center
           px-8
           py-4
-          mt-16
+          mt-14
           border
           shadow-sm
-          text-6xl
+          text-5xl
           font-semibold
           bg-yellow-300
           text-yellow-50
@@ -52,6 +66,13 @@ export const Home = () => {
         >
           Send
         </button>
+      </div>
+      <div className="flex flex-row items-center justify-center mt-10">
+        {displayQuote()}
+        <img alt="robot" src={robot} width="200" />
+      </div>
+      <div className="text-indigo-900 text-xs font-light mt-20">
+        Designed and Built by Megan & Mehr
       </div>
     </div>
   );
